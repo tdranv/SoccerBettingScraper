dd97f091fd6ebfaa0f2ea0b954b1e4723750104a
Todor <andonov.tdr@gmail.com>
2019-05-26 01:38:18 +0300

    adding link to comment

--- SoccerBettingScraper.py
+++ SoccerBettingScraper.py
@@ -1,13 +1,13 @@
 import datetime
-
 import praw
 
 reddit = praw.Reddit(client_id='<id>', \
                      client_secret='<secret>', \
                      user_agent='Scraper')
 
-team = input("Team: ")
-new_posts = list(reddit.subreddit('SoccerBetting').new())
+# team = input("Team: ")
+team = "valencia"
+new_posts = list(reddit.subreddit('SoccerBetting').new(limit=50))
 
 for submission in new_posts:
     submission.comments.replace_more(limit=0)
@@ -21,5 +21,6 @@ for submission in new_posts:
                     if team.lower() in item.lower():
                         print(datetime.datetime.fromtimestamp(submission.created), " || ",
                               submission.title, " || ", item.strip())
+                print("More: " + "https://www.reddit.com" + submission.permalink + str(comment))
 
-print("\nGood luck")
+print("\nGood luck :)")
